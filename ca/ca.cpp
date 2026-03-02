1da68f31da35d3d04ae9c8fd89aa1ea003abc697
Matek <99048772+Matek05l@users.noreply.github.com>
2026-03-02 17:45:53 +0100

    Add files via upload

---
+++ ca/ca.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class trojkat
+{
+public:
+
+	int dla;
+	int dlh;
+
+	trojkat()
+	{
+		cout << "podaj a: \n";
+		cin >> dla;
+		cout << "podaj h: \n";
+		cin >> dlh;
+	}
+
+
+	void licz()
+	{
+		cout << "pole wynosi: " << ((dla * dlh) / 2) << endl;
+	}
+
+};
+
+int main()
+{
+	int ileo = 0;
+	while (true)
+	{
+		
+		trojkat abc;
+		abc.licz();
+		ileo++;
+		cout << "licznik: " << ileo << endl;
+	}
+
+}
